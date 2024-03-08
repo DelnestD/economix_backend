5ba2588c57084ba16a9c40ec97fb85a722bd18ff
R3dn0 <onderpout@gmail.com>
2024-03-08 10:08:22 +0100

    Error: Removing MissingEnvError

--- src/api/error/handler.error.ts
+++ src/api/error/handler.error.ts
@@ -1,10 +1,12 @@
 import { type Request, type Response, type NextFunction } from "express";
-import { EntityNotFoundError, MissingEnvError, NotFoundError } from "./types.error";
+import { EntityNotFoundError, NotFoundError } from "./types.error";
 
-export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
-    if (err instanceof MissingEnvError) {
-        return missingEnvErrorHandler(err, req, res, next);
-    }
+export function errorHandler(
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
     if (err instanceof EntityNotFoundError) {
         return entityNotFoundErrorHandler(err, req, res, next);
     }
@@ -14,16 +16,28 @@ export function errorHandler(err: Error, req: Request, res: Response, next: Next
     return internaleServerErrorHandler(err, req, res, next);
 }
 
-function internaleServerErrorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+function internaleServerErrorHandler(
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
     console.error(err.stack);
     res.status(500).send("Something went wrong with the server");
 }
-function entityNotFoundErrorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+function entityNotFoundErrorHandler(
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
     res.status(404).send("Entity not found");
 }
-function notFoundErrorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+function notFoundErrorHandler(
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
     res.status(404).send("Page not found");
-}
-function missingEnvErrorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
-    res.status(500).send("Missing environment variable");
 }
--- src/api/error/types.error.ts
+++ src/api/error/types.error.ts
@@ -1,5 +1,3 @@
 export class NotFoundError extends Error {}
 
 export class EntityNotFoundError extends Error {}
-
-export class MissingEnvError extends Error {}
