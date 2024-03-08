11e6e9b8ee1b2bf2d4491814a13fc1f5b2e7c3bf
DelnestD <delnest.d@gmail.com>
2024-03-08 10:10:18 +0100

    init routes user and group in api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,16 @@
 import express, { Application, json } from "express";
 import { NotFoundError } from "./error/types.error";
 import { errorHandler } from "./error/handler.error";
+import { userRouter } from "./routes/user.routes";
+import { groupRouter } from "./routes/group.routes";
 
 const PORT = 8081;
 
 export function initApi() {
     const application: Application = express();
     application.use(json());
+    application.use("/user", userRouter);
+    application.use("/group", groupRouter);
     application.use((request, response, next) => {
         throw new NotFoundError();
     });
