96daf21ce0175773a3a9ebfcea721ab68afc6004
DelnestD <delnest.d@gmail.com>
2024-03-08 12:28:11 +0100

    add transaction in seeder

--- src/core/database/seeder.ts
+++ src/core/database/seeder.ts
@@ -2,6 +2,7 @@ import { Role, User } from "../models/user.class";
 import { Group } from "../models/group.class";
 import { DatabaseConnection } from "./connection";
 import { Budget } from "../models/budget.class";
+import { Transaction } from "../models/transaction.class";
 
 export async function seeder() {
     const manager = DatabaseConnection.manager;
@@ -11,36 +12,66 @@ export async function seeder() {
     });
     await manager.save(simpson);
 
+    const [homerAddTransaction, margeAddTransaction, bartAddTransaction, lisaAddTransaction, maggieAddTransaction] = manager.create(Transaction, [
+        {
+            title: "Salaire",
+            amount: 1000,
+            date: new Date("2024-03-08"),
+        },
+        {
+            title: "Argent de Poche",
+            amount: 1000,
+            date: new Date("2024-03-08"),
+        },
+        {
+            title: "Argent de Poche",
+            amount: 100,
+            date: new Date("2024-03-08"),
+        },
+        {
+            title: "Argent de Poche",
+            amount: 100,
+            date: new Date("2024-03-08"),
+        },
+        {
+            title: "Argent de Poche",
+            amount: 300,
+            date: new Date("2024-03-08"),
+        },
+    ]);
+
+    await manager.save([homerAddTransaction, margeAddTransaction, bartAddTransaction, lisaAddTransaction, maggieAddTransaction]);
+
     const [homerAccount, margeAccount, bartAccount, lisaAccount, maggieAccount] = manager.create(Budget, [
         {
             title: "Compte courant",
             description: "compte courant d'homer",
             amount: 1000,
-            transactions: null,
+            transactions: [homerAddTransaction],
         },
         {
             title: "Compte courant",
             description: "compte courant de marge",
             amount: 1000,
-            transactions: null,
+            transactions: [margeAddTransaction],
         },
         {
             title: "Compte courant",
             description: "compte courant de bart",
             amount: 100,
-            transactions: null,
+            transactions: [bartAddTransaction],
         },
         {
             title: "Compte courant",
             description: "compte courant de lisa",
             amount: 100,
-            transactions: null,
+            transactions: [lisaAddTransaction],
         },
         {
             title: "Compte courant",
             description: "compte courant de maggie",
             amount: 300,
-            transactions: null,
+            transactions: [maggieAddTransaction],
         },
     ]);
 
