f1fb975fa4b8aa8af74001709b81e4dc1fa3d101
DelnestD <delnest.d@gmail.com>
2024-03-16 16:49:41 +0100

    add error type for login failed + update response

--- src/api/routes/auth.routes.ts
+++ src/api/routes/auth.routes.ts
@@ -1,63 +1,62 @@
 import bcrypt from "bcrypt";
 import { Router } from "express";
 import { sign } from "jsonwebtoken";
-import { EntityNotFoundError } from "../error/types.error";
-import {
-  getUserByEmail,
-  insertUser,
-} from "../../core/database/utils/user.utils";
-import { stringify } from "querystring";
+import { LoginFailedError } from "../error/types.error";
+import { getUserByEmail, insertUser } from "../../core/database/utils/user.utils";
 
 export const authRouter = Router();
 
 authRouter.post("/login", async (request, response, next) => {
-  const { email, password } = request.body;
+    const { email, password } = request.body;
 
-  const user = await getUserByEmail(email);
+    const user = await getUserByEmail(email);
 
-  if (!user) {
-    return next(new EntityNotFoundError("Mot de passe ou email incorrect"));
-  }
-  if (!(await bcrypt.compare(password, user.password))) {
-    return next(new EntityNotFoundError("Mot de passe ou email incorrect"));
-  }
+    if (!user) {
+        return next(new LoginFailedError("Email ou mot de passe incorrect"));
+    }
+    if (!(await bcrypt.compare(password, user.password))) {
+        return next(new LoginFailedError("Email ou mot de passe incorrect"));
+    }
 
-  const accessToken = sign(
-    {
-      id: user.id,
-      iat: Math.floor(new Date().getTime() / 1000),
-      exp: Math.floor(new Date().getTime() / 1000 + 300),
-    },
-    process.env.JWT_SECRET!
-  );
+    const expireDate = new Date(Date.now() + 300000);
 
-  response.cookie("accessToken", accessToken, {
-    expires: new Date(Date.now() + 300000),
-    httpOnly: true,
-  });
+    const accessToken = sign(
+        {
+            id: user.id,
+            iat: Math.floor(new Date().getTime() / 1000),
+            exp: Math.floor(new Date().getTime() / 1000 + 300),
+        },
+        process.env.JWT_SECRET!
+    );
 
-  response.send({
-    accessToken,
-  });
+    response.cookie("accessToken", accessToken, {
+        expires: expireDate,
+        httpOnly: true,
+    });
+
+    response.send({
+        accessToken,
+        expires: expireDate,
+    });
 });
 
 authRouter.post("/register", async (request, response, next) => {
-  const body = request.body;
+    const body = request.body;
 
-  try {
-    await getUserByEmail(body.email);
-    response.status(409).json({
-      message: "Mail exists",
-    });
-  } catch (error) {
-    bcrypt.hash(body.password, 10, (err, hash) => {
-      if (err) {
-        return response.status(500);
-      } else {
-        body.password = hash;
-        insertUser(body);
-        response.status(200).send(body);
-      }
-    });
-  }
+    try {
+        await getUserByEmail(body.email);
+        response.status(409).json({
+            message: "Mail exists",
+        });
+    } catch (error) {
+        bcrypt.hash(body.password, 10, (err, hash) => {
+            if (err) {
+                return response.status(500);
+            } else {
+                body.password = hash;
+                insertUser(body);
+                response.status(200).send(body);
+            }
+        });
+    }
 });
