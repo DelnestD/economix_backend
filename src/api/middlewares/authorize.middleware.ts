089b4946c91a24692a7c709240e59c3ac8ddcbeb
R3dn0 <onderpout@gmail.com>
2024-03-21 17:56:10 +0100

    fix authmiddleware

--- src/api/middlewares/authorize.middleware.ts
+++ src/api/middlewares/authorize.middleware.ts
@@ -4,32 +4,34 @@ import { verify } from "jsonwebtoken";
 
 export function authorizeMiddleWare(roles: string[]): RequestHandler {
     return async (request: Request, response: Response, next: NextFunction) => {
-        // const token = request.cookies.accessToken;
-        // console.log(token);
+        const token = request.headers.authorization;
 
-        // if (!token) {
-        //     return response.status(401).send("Unauthorized");
-        // }
+        if (!token) {
+            return response.status(401).send("Unauthorized");
+        }
 
-        // try {
-        //     const accessTokenDecoded = verify(token, process.env.JWT_SECRET!, {
-        //         complete: true,
-        //     });
-        //     console.log(accessTokenDecoded);
+        try {
+            const accessTokenDecoded = verify(token, process.env.JWT_SECRET!, {
+                complete: true,
+            });
 
-        //     const user = await getUserById(accessTokenDecoded.payload.sub as string);
-        //     console.log(user);
+            const user = await getUserById(
+                accessTokenDecoded.payload.sub as string
+            );
 
-        //     if (!user) {
-        //         return response.status(403).send("Forbidden");
-        //     }
+            if (!user) {
+                return response.status(403).send("Forbidden");
+            }
 
-        //     if (!user.role || (roles.length > 0 && !roles.includes(user.role))) {
-        //         return response.status(403).send("Forbidden");
-        //     }
-        // } catch (e) {
-        //     return response.status(401).send("Unauthorized");
-        // }
+            if (
+                !user.role ||
+                (roles.length > 0 && !roles.includes(user.role))
+            ) {
+                return response.status(403).send("Forbidden");
+            }
+        } catch (e) {
+            return response.status(401).send("Unauthorized");
+        }
 
         next();
     };
